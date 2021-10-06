6e252b1a30a048d483e40d717f20d3c99540c4ee
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-06 21:57:05 +0530

    Add files via upload

---
+++ Maxrobot/modules/covid.py
@@ -0,0 +1,32 @@
+import requests
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+
+from Maxrobot import dispatcher
+from Maxrobot.modules.disable import DisableAbleCommandHandler
+
+
+@run_async
+def covid(update: Update, context: CallbackContext):
+    message = update.effective_message
+    text = message.text.split(" ", 1)
+    if len(text) == 1:
+        r = requests.get("https://corona.lmao.ninja/v2/all").json()
+        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+    else:
+        variabla = text[1]
+        r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
+        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+
+COVID_HANDLER = DisableAbleCommandHandler(["covid", "corona"], covid)
+dispatcher.add_handler(COVID_HANDLER)
+
+__help__ = """
+@max123robot🇱🇰
+ ❍ /covid - To Get Global Stats of Covid.
+ ❍ /covid <country> - To Get Stats of A Single Country.
+"""
+
+__mod_name__ = "COVID"
---
+++ Maxrobot/modules/ex.py
@@ -0,0 +1,8 @@
+__help__ =  """
+@max123robot
+** Telegraph ** 
+  ❍ tm :Get Telegraph Link Of Replied Media
+  ❍ txt :Get Telegraph Link of Replied Text
+ I can upload files to Telegraph
+"""
+__mod_name__ = "Telegraph🏞"       
