d7f227fcdaaca03adb3c86a1fff9d9475de582fc
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-07 13:12:03 +0530

    Update __init__.py

--- Maxrobot/__init__.py
+++ Maxrobot/__init__.py
@@ -90,6 +90,7 @@ if ENV:
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    bot_start_time = time.time()
 
     try:
         WHITELIST_CHATS = set(
