d8692981710c86b0437976ee30ae484ec7856f26
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-07 11:37:45 +0530

    Create system_stats.py

---
+++ Maxrobot/modules/system_stats.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+import subprocess
+import time
+
+import psutil
+from pyrogram import filters
+
+from Maxrobot import bot_start_time
+from Maxrobot.utils import formatter
+
+# Stats Module
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - bot_start_time)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    stats = f"""
+root@maxsupun:~$ Maxrobot
+------------------
+UPTIME: {formatter.get_readable_time((bot_uptime))}
+BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+CPU: {cpu}%
+RAM: {mem}%
+DISK: {disk}%
+"""
+    return stats
