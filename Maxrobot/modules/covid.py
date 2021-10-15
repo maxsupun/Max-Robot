aa88aa6028430a620930c59c3468c5e779def190
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-15 20:27:44 +0530

    Update covid.py

--- Maxrobot/modules/covid.py
+++ Maxrobot/modules/covid.py
@@ -11,11 +11,11 @@ def covid(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text.split(" ", 1)
     if len(text) == 1:
-        r = requests.get("https://corona.lmao.ninja/v2/all").json()
+        r = requests.get("https://hpb.health.gov.lk/api/get-current-statistical").json()
         reply_text = f"**Global Totals** [🦠](https://telegra.ph/file/47880b9d0a3363ea8ab7b.jpg)\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
-        r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
+        r = requests.get(f"https://hpb.health.gov.lk/api/get-current-statistical/{variabla}").json()
         reply_text = f"**Cases for {r['country']}** [🦠](https://telegra.ph/file/47880b9d0a3363ea8ab7b.jpg)\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
