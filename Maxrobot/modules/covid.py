94cc448d572e258ef29a43eeba52600b927425b1
sᴀᴅᴇᴡ ᴊᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-10-16 05:06:15 -0700

    Update covid.py

--- Maxrobot/modules/covid.py
+++ Maxrobot/modules/covid.py
@@ -12,21 +12,13 @@ def covid(update: Update, context: CallbackContext):
     text = message.text.split(" ", 1)
     if len(text) == 1:
         r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Global Totals** [🦠](https://telegra.ph/file/47880b9d0a3363ea8ab7b.jpg)\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Global Results** 🦠\nConfirmed🌡: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths⚰️: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered♻️: {r['recovered']:,}\nActive🩸: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
-        reply_text = f"**Cases for {r['country']}** [🦠](https://telegra.ph/file/47880b9d0a3363ea8ab7b.jpg)\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Results for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths⚰️: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered♻️: {r['recovered']:,}\nActive🩸: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
 COVID_HANDLER = DisableAbleCommandHandler(["covid", "corona"], covid)
 dispatcher.add_handler(COVID_HANDLER)
-
-__help__ = """
-@max123robot🇱🇰
- ❍ /covid - To Get Global Stats of Covid.
- ❍ /covid <country> - To Get Stats of A Single Country.
-"""
-
-__mod_name__ = "COVID"
