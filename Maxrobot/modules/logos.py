02074f21eb09c8ca1494ef340954b3b6eb4b1f87
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-17 21:44:10 +0530

    Update logos.py

--- Maxrobot/modules/logos.py
+++ Maxrobot/modules/logos.py
@@ -147,6 +147,44 @@ async def lego(event):
             os.remove(fname2)
  except Exception as e:
    await event.reply(f'Join with us ~ @SL_Tech_Worldchat to use this, {e}')
+    
+  @register(pattern="^/nlogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+
+    if not quew:
+       await event.reply('Provide Some Text To Draw!')
+       return
+    else:
+       pass
+ await event.reply('Wait..now ok!')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open(random.choice(TELEGRAPH_MEDIA_LINKS))
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 25
+    fillcolor = "white"
+    shadowcolor = "Red"
+    font = ImageFont.truetype("./Maxrobot/resources/Neon machine.ttf",40)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=2, stroke_fill="yellow")
+    fname2 = "Logo.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Max123robot🇱🇰")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Join with us ~ @SL_Tech_Worldchat to use this, {e}')
+  
   
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
