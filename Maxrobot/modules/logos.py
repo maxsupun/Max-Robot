c5a1aad36d03ced489ab7fc9fe2e2de470bda9bb
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-17 23:35:51 +0530

    Update logos.py

--- Maxrobot/modules/logos.py
+++ Maxrobot/modules/logos.py
@@ -165,7 +165,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Maxrobot/resources/Maghrib.ttf", 50)
+    font = ImageFont.truetype("./Maxrobot/resources/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
