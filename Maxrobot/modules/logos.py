0d608cc4b589119fd6606eb1e1705f542a5fcf11
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-18 20:18:36 +0530

    Update logos.py

--- Maxrobot/modules/logos.py
+++ Maxrobot/modules/logos.py
@@ -50,12 +50,12 @@ async def lego(event):
        pass
  await event.reply('Creating your logo...wait!')
  try:
-    text = event.pattern_match.group(1)
+     text = event.pattern_match.group(1)
     img = Image.open(random.choice(TELEGRAPH_MEDIA_LINKS))
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
+    pointsize = 25
+    fillcolor = "gold"
     shadowcolor = "blue"
     font = ImageFont.truetype("./Maxrobot/resources/font.otf",40)
     w, h = draw.textsize(text, font=font)
@@ -64,52 +64,14 @@ async def lego(event):
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=5, stroke_fill="red")
-    fname2 = "max123robot.png"
+    draw.text((x, y), text, font=font, fill="black", stroke_width=2, stroke_fill="yellow")
+    fname2 = "Logo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @max123robot🇱🇰")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @Max123robot🇱🇰")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Join with us ~ @SL_Tech_Worldchat to use this, {e}')
-    
-    
-@register(pattern="^/nlogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open(random.choice(TELEGRAPH_MEDIA_LINKS))
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./Maxrobot/resources/Neon machine.otf.ttf", 50)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=5, stroke_fill="red")
-    fname2 = "max123robot.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @max123robot🇱🇰")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Join with us ~ @SL_Tech_Worldchat to use this, {e}')
+ await event.reply(f'Join with us ~ @SL_Tech_Worldchat to use this, {e}')
 
 @register(pattern="^/wlogo ?(.*)")
 async def lego(event):
@@ -148,7 +110,7 @@ async def lego(event):
  except Exception as e:
    await event.reply(f'Join with us ~ @SL_Tech_Worldchat to use this, {e}')
 
-@register(pattern="^carbon ?(.*)")
+@register(pattern="^/spiderlogo ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id == OWNER_ID:
@@ -163,7 +125,7 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./Maxrobot/resources/photo_2021-08-21_23-18-11.jpg')
+    img = Image.open('./Maxrobot/resources/spiderbg (2).png')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
@@ -232,7 +194,7 @@ __help__ = """
 @max123robot🇱🇰
  ❍ /logo text :  Create your logo with your name
  ❍ /wlogo text :  Create your logo with your name
- ❍ /carbon  text :  Create your logo with your name
+ ❍ /spiderlogo  text :  Create your logo with your name
  ❍ /pandalogo :  Create your logo with your name
  """
 __mod_name__ = "Logo Maker"
