aab25600f1418aef293855465ce3aabb46255271
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-19 22:29:44 +0530

    Create ex1.py

---
+++ Maxrobot/modules/ex1.py
@@ -0,0 +1,13 @@
+__help__ =  """
+@max123robot🇱🇰
+** Styletext ** 
+ ❍ /weebify
+ ❍ /bubble
+ ❍ /fbubble
+ ❍ /fsquare
+ ❍ /blue
+ ❍ /latin
+ ❍ /lined
+ ❍ /square
+"""
+__mod_name__ = "Styletext〽️"       
