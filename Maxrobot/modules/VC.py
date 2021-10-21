f793d218535fd38288fca36ca8d5230c4c4f2b20
Supun Max <85874927+maxsupun@users.noreply.github.com>
2021-10-21 21:47:11 +0530

    Create VC.py

---
+++ Maxrobot/modules/VC.py
@@ -0,0 +1,59 @@
+
+__help__ =  """
+@Max123Robot🇱🇰
+
+** VC Music Play🎶 ** 
+  Commands 
+  
+❍/play <song name> - play song you requested
+❍/dplay <song name> - play song you requested via deezer
+❍/splay <song name> - play song you requested via jio saavn
+❍/playlist - Show now playing list
+❍/current - Show now playing
+❍/song <song name> - download songs you want quickly
+❍/search <query> - search videos on youtube with details
+❍/deezer <song name> - download songs you want quickly via deezer
+❍/saavn <song name> - download songs you want quickly via saavn
+❍/video <song name> - download videos you want quickly
+
+Admins only.
+
+❍/player - open music player settings panel
+❍/pause - pause song play
+❍/resume - resume song play
+❍/skip - play next song
+❍/end - stop music play
+❍/userbotjoin - invite assistant to your chat
+❍/userbotleave - remove assistant from your chat
+❍/admincache - Refresh admin list
+
+Commands for Channel Music Play 
+For linked group admins only:
+
+❍/cplay <song name> - play song you requested
+❍/cplay <reply to link> - play replied youtube link
+❍/cplay <reply to audio> - play replied file
+❍/cdplay <song name> - play song you requested via deezer
+❍/csplay <song name> - play song you requested via jio saavn
+❍/cplaylist - Show now playing list
+❍/cccurrent - Show now playing
+❍/cplayer - open music player settings panel
+❍/cpause - pause song play
+❍/cresume - resume song play
+❍/cskip - play next song
+❍/cend - stop music play
+❍/userbotjoinchannel - invite assistant to your chat
+ 
+"""
+repmark = InlineKeyboardMarkup(
+      [
+        [
+        InlineKeyboardButton(text="Official Bots updates", url=f"https://t.me/szteambots") 
+        ],
+        [
+         InlineKeyboardButton(text="Official Bots support", url=f"https://t.me/slbotzone") 
+        ]
+      ]      
+    )
+__mod_name__ = "VC Music Play🎶 "   
+
